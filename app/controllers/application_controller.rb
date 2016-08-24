c56e7d739c364ec207bbf2b4b181b00715c3954a
Celine Colas <colasceline88@gmail.com>
2016-08-24 15:46:24 +0200

    Fix conflicts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    if resource.teacher == true
+      teacher_profile_path
+    else
+      student_profile_path
+    end
+  end
+
 end
