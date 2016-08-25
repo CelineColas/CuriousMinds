9364e5837c08c33afe2eada5958bbeba25d4e8d9
Celine Colas <colasceline88@gmail.com>
2016-08-25 17:01:08 +0200

    Questions view are better but not totally fixed yet

--- app/assets/javascripts/student/card.js
+++ app/assets/javascripts/student/card.js
@@ -1,6 +1,40 @@
-  $('.flip').click(function(){
-      $(this).find('.card').addClass('flipped').mouseleave(function(){
-          $(this).removeClass('flipped');
-      });
-      return true;
-  });
+// var in_box = false;
+
+// $('.front').click(function(){
+
+//   $("#effort_content").click(function() {
+//     console.log("YAY !!");
+//     var in_box = true;
+//   });
+
+//   console.log(in_box);
+//   if (in_box == true) {
+//     console.log("gagné !");
+//     return true;
+//   }
+//   else {
+//     if ($('.card').hasClass("flipped")) {
+//       $(this).find('.card').removeClass('flipped');
+//     }
+//     else {
+//       $(this).find('.card').addClass('flipped');
+//     }
+//     return true;
+//   }
+// });
+
+
+
+
+
+$('.front').click(function(){
+  $('.flip > .card:not(.question-input-field').addClass('flipped');
+});
+
+$('.wrapper').click(function(event){
+  event.preventDefault();
+  $('.flip > .card').removeClass('flipped');
+});
+
+
+
